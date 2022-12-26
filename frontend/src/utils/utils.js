2c552b5357d6fd19c017b8bdc933aba620d8bc9e
vlad <vladshpilka86@gmail.com>
2022-12-26 15:08:54 +0200

    fix linters error

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -42,7 +42,15 @@ export const prepareHtmlForDoc = htmlString => {
 
 export const exportToWord = element => {
     const preHtml =
-        "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+        '<html' +
+        "xmlns:o='urn:schemas-microsoft-com:office:office'" +
+        "xmlns:w='urn:schemas-microsoft-com:office:word'" +
+        "xmlns='http://www.w3.org/TR/REC-html40'>" +
+        '<head>' +
+        "<meta charset='utf-8'>" +
+        '<title>Export HTML To Doc</title>' +
+        '</head>' +
+        '<body>';
     const postHtml = '</body></html>';
     const html = preHtml + element + postHtml;
     const blob = new Blob(['\ufeff', html], {

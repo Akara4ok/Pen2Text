a71b34d3a137a97fae3cdf24194d72c317b48402
vlad <vladshpilka86@gmail.com>
2022-12-26 04:39:17 +0200

    add file saving

--- frontend/src/Components/TextEditor/TextEditor.jsx
+++ frontend/src/Components/TextEditor/TextEditor.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import classes from './TextEditor.scss';
 import Button from '@Components/Button/Button';
 import DropdownList from '@Components/DropdownList/DropdownList';
+import Backdrop from '@Components/Backdrop/Backdrop';
+import Spinner from '@Components/Spinner/Spinner';
 import { Editor } from 'react-draft-wysiwyg';
 import { EditorState, ContentState } from 'draft-js';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { convertToRaw } from 'draft-js';
 import htmlToDraft from 'html-to-draftjs';
-import { buildHtmlFromResponse } from '../../utils/utils';
+import draftToHtml from 'draftjs-to-html';
+import {
+    buildHtmlFromResponse,
+    prepareHtmlForDoc,
+    exportToWord,
+    saveFile,
+} from '../../utils/utils';
+import jsPDF from 'jspdf';
 
 class TextEditor extends React.Component {
     constructor(props) {
@@ -16,6 +25,7 @@ class TextEditor extends React.Component {
             plainText: [],
             downloadType: '*.txt',
             editorState: EditorState.createEmpty(),
+            isSaving: false,
         };
     }
 
@@ -28,8 +38,7 @@ class TextEditor extends React.Component {
         ) {
             return;
         }
-        const htmlView = buildHtmlFromResponse(plainText).innerHTML ;
-        console.log(htmlView);
+        const htmlView = buildHtmlFromResponse(plainText).innerHTML;
         const contentBlock = htmlToDraft(htmlView);
         if (contentBlock) {
             const contentState = ContentState.createFromBlockArray(
@@ -46,12 +55,119 @@ class TextEditor extends React.Component {
         });
     };
 
-    setValue = (type) => {
-        this.setState({downloadType: type});
-    }
+    onDownloadClick = () => {
+        const { downloadType } = this.state;
+        const filename = 'result';
+        if (downloadType === '*.txt') {
+            this.saveTxtFile(filename);
+        } else if (downloadType === '*.docx') {
+            this.saveDocFile(filename);
+        } else if (downloadType === '*.pdf') {
+            this.savePDFFile(filename);
+        }
+    };
+
+    saveTxtFile = filename => {
+        const { editorState } = this.state;
+        const blocks = convertToRaw(editorState.getCurrentContent()).blocks;
+        const value = blocks
+            .map(block => (!block.text.trim() && '\n') || block.text)
+            .join('\n');
+        const file = new Blob([value], { type: 'text/plain' });
+        const saveOpts = {
+            suggestedName: filename + '.txt',
+            types: [
+                {
+                    description: 'Text file',
+                    accept: { 'text/plain': ['.txt'] },
+                },
+            ],
+        };
+        saveFile(file, saveOpts);
+    };
+
+    saveDocFile = filename => {
+        const { editorState } = this.state;
+        const rawContentState = convertToRaw(editorState.getCurrentContent());
+        const markup = draftToHtml(rawContentState);
+        const sanitizeHTML = prepareHtmlForDoc(markup);
+        const file = exportToWord(sanitizeHTML);
+        const saveOpts = {
+            suggestedName: filename + '.docx',
+            types: [
+                {
+                    description: 'Doc file',
+                    accept: { 'application/msword': ['.docx'] },
+                },
+            ],
+        };
+        saveFile(file, saveOpts);
+    };
+
+    savePDFFile = filename => {
+        const { editorState } = this.state;
+        const rawContentState = convertToRaw(editorState.getCurrentContent());
+        const markup = draftToHtml(rawContentState);
+        const doc = new jsPDF();
+
+        const separator = '<p>' + '-'.repeat(50) + '</p>\n';
+        const pages = markup.split(separator);
+        console.log(pages);
+
+        let index = 0;
+
+        const saveCalback = doc => {
+            const blobPDF = new Blob([doc.output('blob')], {
+                type: 'application/pdf',
+            });
+            saveFile(blobPDF, saveOpts);
+            this.setState({ isSaving: false });
+        };
+
+        const height = doc.internal.pageSize.getHeight();
+
+        const createPageCallback = doc => {
+            let element = pages[index];
+            const pageCount = doc.internal.getNumberOfPages() - 1;
+            console.log(pageCount);
+            doc.html(element, {
+                callback:
+                    index === pages.length - 1
+                        ? saveCalback
+                        : createPageCallback,
+                x: 0,
+                y: 0 + (height - 30) * pageCount,
+                width: 170,
+                windowWidth: 650,
+                margin: [15, 15],
+                autoPaging: 'text',
+            });
+            if (index < pages.length - 1) {
+                doc.addPage();
+                index++;
+            }
+        };
+
+        const saveOpts = {
+            suggestedName: filename + '.pdf',
+            types: [
+                {
+                    description: 'PDF file',
+                    accept: { 'application/pdf': ['.pdf'] },
+                },
+            ],
+        };
+
+        this.setState({ isSaving: true });
+        createPageCallback(doc);
+    };
+
+    setValue = type => {
+        this.setState({ downloadType: type });
+    };
 
     render() {
-        const { editorState } = this.state;
+        const { editorState, isSaving } = this.state;
         return (
             <div className={classes.wrapper}>
                 <Editor
@@ -71,6 +187,11 @@ class TextEditor extends React.Component {
                     onClick={this.onDownloadClick}>
                     Download
                 </Button>
+                {isSaving ? (
+                    <Backdrop>
+                        <Spinner />
+                    </Backdrop>
+                ) : null}
             </div>
         );
     }
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -24,3 +24,43 @@ export const buildHtmlFromResponse = texts => {
     }
     return content;
 };
+
+export const prepareHtmlForDoc = htmlString => {
+    let content = htmlString;
+    let currentPos = 0;
+    const separator = '<p>' + '-'.repeat(50) + '</p>';
+    while (content.indexOf(separator, currentPos) !== -1) {
+        currentPos = content.indexOf(separator, currentPos) + separator.length;
+        content =
+            content.slice(0, currentPos) +
+            ' <br style="page-break-before: always"> ' +
+            content.slice(currentPos);
+    }
+    content = content.replaceAll(separator, '');
+    return content;
+};
+
+export const exportToWord = element => {
+    const preHtml =
+        "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+    const postHtml = '</body></html>';
+    const html = preHtml + element + postHtml;
+    const blob = new Blob(['\ufeff', html], {
+        type: 'application/msword',
+    });
+
+    return blob;
+};
+
+export const saveFile = (content, opts) => {
+    window
+        .showSaveFilePicker(opts)
+        .then(fileHandler => {
+            fileHandler.createWritable().then(writable => {
+                writable.write(content).then(() => {
+                    writable.close();
+                });
+            });
+        })
+        .catch(() => {});
+};
